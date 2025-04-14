42bc3dfe126700e1aa013c222041b079a7174447
tasyadwii <145946797+tasyadwii@users.noreply.github.com>
2025-04-14 10:34:21 +0800

    Add files via upload

---
+++ pre-pratikum/pre-2.sql
@@ -0,0 +1,18 @@
+-- Nama : Tasya dw rahma dani 
+-- NIM : 23241028
+-- KELAS : A
+
+-- melihat Database yang digunakan
+USE mysql;
+
+-- mendeskripsikan tabel user 
+DESCRIBE user;
+
+-- menampilkan tabel dalam data base mysql
+SHOW TABLES;
+
+-- mendeskripsikan tabel user
+DESCRIBE user;
+
+-- menampilkan isi data dari kolom Host,User,Password
+SELECT Host , User , Pasword FROM user;
